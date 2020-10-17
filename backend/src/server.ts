f8d8b2973ded030b965bb40bba10a98ccf963309
Rogerio Ferreira <rogeriosoaresec@gmail.com>
2020-10-17 13:11:18 +0100

    adicionando o CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,16 +4,17 @@ import 'express-async-errors';
 import routes from './routes';
 import path from 'path';
 
+import cors from 'cors';
+
 import './database/connection';
 
 import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.get('/', (req, res) => {
