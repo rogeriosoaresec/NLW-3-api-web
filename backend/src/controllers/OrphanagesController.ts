f781f8c84d1b7a40ff859fa95bd5305ac1bd1ab9
Rogerio Ferreira <rogeriosoaresec@gmail.com>
2020-10-17 02:06:17 +0100

    upload de imagens

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,22 +1,24 @@
 import {Request, Response} from "express";
 import { getRepository } from 'typeorm';
 import Orphanages from '../models/Orphanage';
+import orphanageView from '../views/orphanages_view';
+
 
 export default {
     async index(req:Request, res:Response) {
         const orphanagesRepository = getRepository(Orphanages);
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({ relations: [ "images" ] });
 
-        return res.json(orphanages);
+        return res.json(orphanageView.renderMany(orphanages));
     }, 
 
     async show(req:Request, res:Response) {
         const { id } = req.params;
 
         const orphanagesRepository = getRepository(Orphanages);
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, { relations: [ "images" ] });
 
-        return res.json(orphanage);
+        return res.json(orphanageView.render(orphanage));
     }, 
 
     async create(req:Request, res:Response) {
@@ -51,6 +53,6 @@ export default {
         await orphanagesRepository.save(orphanage);
 
         
-        return res.status(201).json(orphanage);
+        return res.status(201).json(orphanageView.render(orphanage));
     }
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import './database/connection';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.get('/', (req, res) => {
     return res.json({ welcome: "Implementação de API" })
