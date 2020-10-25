5eab7ef2ff4a4f7b7cc7bf920f402455d65ca978
Rogerio Ferreira <rogeriosoaresec@gmail.com>
2020-10-25 12:44:58 +0000

    finalizar o front

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -47,7 +47,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends === 'true',
             images    
         };
 
